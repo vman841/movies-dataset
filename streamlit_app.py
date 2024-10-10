25e37b388faa086a8b33bccbe06bf29d1b474a59
vman841 <64185461+vman841@users.noreply.github.com>
2024-10-10 12:53:08 +0000

    added cricket test bat dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Cricket dataset", page_icon="🎬")
+st.title("🎬 Cricket dataset")
 st.write(
     """
     This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
@@ -18,7 +18,7 @@ st.write(
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+    df = pd.read_csv("data/CrickInfo_New - Test_Bat.csv")
     return df
 
 
@@ -26,40 +26,43 @@ df = load_data()
 
 # Show a multiselect widget with the genres using `st.multiselect`.
 genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
+    "Team",
+    df.Team.unique(),
+    ["ENG", "WI", "AUS", "NZ", "IND", "SA", "SL", "PAK", "ZIM", "BAN", "IRE", "AFG"],
 )
 
 # Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
+years = st.slider("Matches", 1, 200, (1, 16))
 
 # Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+df_filtered = df[(df["Team"].isin(genres)) & (df["Mat"].between(years[0], years[1]))]
 df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+    index="StartYear", columns="Team", values="Runs", aggfunc="sum", fill_value=0
 )
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
+df_reshaped = df_reshaped.sort_values(by="StartYear", ascending=False)
+cop = len(pd.unique(df_filtered['PlayerName']))
+print(cop)
+st.metric(label="CountOfPlayers",value=cop)
+st.table(df_filtered)
 
 # Display the data as a table using `st.dataframe`.
 st.dataframe(
     df_reshaped,
     use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
+    column_config={"StartYear": st.column_config.TextColumn("StartYear")},
 )
 
 # Display the data as an Altair chart using `st.altair_chart`.
 df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
+    df_reshaped.reset_index(), id_vars="StartYear", var_name="Team", value_name="Runs"
 )
 chart = (
     alt.Chart(df_chart)
     .mark_line()
     .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
+        x=alt.X("StartYear:N", title="Year"),
+        y=alt.Y("Runs:Q", title="Gross earnings ($)"),
+        color="Team:N",
     )
     .properties(height=320)
 )
